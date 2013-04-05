ed99e28ecf9c585c1850b41fe58dd3f9e1b65bb1
tobitobita <tobi_x2@yahoo.co.jp>
2013-04-05 18:53:41 +0900

    定数の移動

--- src/main/java/dsk/tweet_a_gram/core/Const.java
+++ src/main/java/dsk/tweet_a_gram/core/Const.java
@@ -8,7 +8,4 @@ public final class Const {
 	public static long AUTH_ERROR = 1;
 	/** ダイヤグラム画像生成エラー */
 	public static long NO_CREATE_IMAGE_FILE = 2;
-
-	/** リソースファイルのキー */
-	public static String MESSAGE = "tweet-a-gram";
 }
